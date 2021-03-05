6bcbfe07b32e74b41a61f308b1acbd97db734792
SkylerElmstrom <47929787+SkylerElmstrom@users.noreply.github.com>
2021-03-05 12:39:43 -0700

    Initial changes to test package

--- R/unzipShape.r
+++ R/unzipShape.r
@@ -1,18 +1,9 @@
-###################################################################################################################################
-# Unzip Shapefile Function
-# Author: Skyler Elmstrom
-# Date: 2021-03-05
-# R Package Requirements: sf package
-# Description: This function is designed to download zipped shapefiles directly from GitHub or other Zip Shapefile download links,
-# store them in a temporary directory, and load the shapefile as an sf object in R.
-###################################################################################################################################
-
-source(unzipShape <- function(InputZipShapefile){
+unzipShape <- function(InputZipShapefile){
   requireNamespace("sf", quietly = TRUE)
-  dl.temp <- tempfile() # Create local temp file for zipped shapefile
-  dl.temp2 <- tempfile() # Create a second local temp file to store unzipped shapefile
-  download.file(InputZipShapefile, dl.temp, quiet=T) # Downloads zip file from InputShape
-  unzip(zip = dl.temp, exdir = dl.temp2) # Unzips zip file
-  shapefile.out <-list.files(dl.temp2, pattern = ".shp$",full.names=TRUE) # stores file path of files with .shp ext in dl.temp2
-  sf::st_read(shapefile.out) # Reads shapefile as sf object
-})
+  dl.temp <- tempfile()
+  dl.temp2 <- tempfile()
+  download.file(InputZipShapefile, dl.temp, quiet=T)
+  unzip(zip = dl.temp, exdir = dl.temp2)
+  shapefile.out <-list.files(dl.temp2, pattern = ".shp$",full.names=TRUE)
+  sf::st_read(shapefile.out)
+}
