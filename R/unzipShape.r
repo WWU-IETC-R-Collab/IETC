7ba2fdb00524f94fc8a22787883fa6034ecf38de
SkylerElmstrom <skylerelmstrom@users.noreply.github.com>
2021-03-05 12:32:00 -0800

    dev 9001 - Update unzipShape function with namespace calls

--- R/unzipShape.r
+++ R/unzipShape.r
@@ -1,9 +1,19 @@
+#' Unzip zipped shapefiles from a download web address such as a raw GitHub link
+#'
+#' @title IETC
+#' @param InputZipShapefile Web address of a web-accessible zippped shapefile
+#' @return sf object loaded from input shapefile
+#' @export
+
 unzipShape <- function(InputZipShapefile){
-  requireNamespace("sf", quietly = TRUE)
+  if (!requireNamespace(c("sf"), quietly = TRUE)) {
+    stop("Package \"sf\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
   dl.temp <- tempfile()
   dl.temp2 <- tempfile()
-  download.file(InputZipShapefile, dl.temp, quiet=T)
-  unzip(zip = dl.temp, exdir = dl.temp2)
+  utils::download.file(InputZipShapefile, dl.temp, quiet=T)
+  utils::unzip(zip = dl.temp, exdir = dl.temp2)
   shapefile.out <-list.files(dl.temp2, pattern = ".shp$",full.names=TRUE)
   sf::st_read(shapefile.out)
 }
